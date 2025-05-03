86a51d98a440d7b59f00fa8dbd12d6cbf182e680
muniprasad <muniprasaddiddi@gmail.com>
2025-05-03 10:43:23 +0530

    files updated

--- tfstate.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    subscription_id      = "aa32da49-0603-4855-b55b-bfd4bcf7b16f"
-    resource_group_name  = "project_rg"
-    storage_account_name = "terratfstateform"
-    container_name       = "roboshop-tfstate-files"
-    key                  = "test.terraform.tfstate"
-  }
-}
