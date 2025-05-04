d65e4e9ac41c59505915f748d095833609b00109
muniprasad <muniprasaddiddi@gmail.com>
2025-05-04 07:14:28 +0530

    files updated

--- toolsetup/vm/main.tf
+++ toolsetup/vm/main.tf
@@ -2,6 +2,7 @@ resource "azurerm_public_ip" "public-ip" {
   name                = var.name
   resource_group_name = var.rg_name
   location            = var.location
+  sku                 = "Standard"
   allocation_method   = "Dynamic"
 }
 
