ae5fdf12dec5aa4de96459f7c6a84b46e338216a
muniprasad <muniprasaddiddi@gmail.com>
2025-05-04 07:01:34 +0530

    files updated

--- toolsetup/vm/main.tf
+++ toolsetup/vm/main.tf
@@ -1,11 +1,11 @@
-resource "azurerm_public_ip" "publicip" {
+resource "azurerm_public_ip" "public-ip" {
   name                = var.name
   resource_group_name = var.rg_name
   location            = var.location
   allocation_method   = "Dynamic"
 }
 
-resource "azurerm_network_interface" "privateip" {
+resource "azurerm_network_interface" "private-ip" {
   name                = "${var.name}-nic"
   location            = var.location
   resource_group_name = var.rg_name
@@ -14,12 +14,12 @@ resource "azurerm_network_interface" "privateip" {
     name                          = var.name
     subnet_id                     = "/subscriptions/aa32da49-0603-4855-b55b-bfd4bcf7b16f/resourceGroups/project_rg/providers/Microsoft.Network/virtualNetworks/project_vn/subnets/default"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.publicip.id
+    public_ip_address_id          = azurerm_public_ip.public-ip.id
   }
 }
 
 resource "azurerm_network_interface_security_group_association" "nsg" {
-  network_interface_id      = azurerm_network_interface.privateip.id
+  network_interface_id      = azurerm_network_interface.private-ip.id
   network_security_group_id = "/subscriptions/aa32da49-0603-4855-b55b-bfd4bcf7b16f/resourceGroups/project_rg/providers/Microsoft.Network/networkSecurityGroups/project_nsg"
 }
 
@@ -27,7 +27,7 @@ resource "azurerm_virtual_machine" "vm" {
   name                  = "${var.name}-vm"
   location              = var.location
   resource_group_name   = var.rg_name
-  network_interface_ids = [azurerm_network_interface.privateip.id]
+  network_interface_ids = [azurerm_network_interface.private-ip.id]
   vm_size               = var.vm_size
 
 delete_os_disk_on_termination = true
