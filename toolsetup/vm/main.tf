7728513ec685d2fca6c2ce9ac764150e535e3102
muniprasad <muniprasaddiddi@gmail.com>
2025-05-05 12:56:55 +0530

    roboshop project update

--- toolsetup/vm/main.tf
+++ toolsetup/vm/main.tf
@@ -112,7 +112,10 @@ resource "azurerm_dns_a_record" "private_dns_record" {
 }
 
 resource "azurerm_dns_a_record" "public_dns_record" {
-  depends_on          = [azurerm_linux_virtual_machine.vm]
+  depends_on          = [
+    azurerm_linux_virtual_machine.vm
+    azurerm_public_ip.public-ip
+  ]
   name                = "${var.name}-dev"
   zone_name           = "prasaddevops.shop"
   resource_group_name = var.rg_name
