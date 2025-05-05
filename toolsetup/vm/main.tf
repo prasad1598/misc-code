33284b31c0629b9e63e7f842c4ff6f83682ce8be
muniprasad <muniprasaddiddi@gmail.com>
2025-05-05 12:34:18 +0530

    roboshop project update

--- toolsetup/vm/main.tf
+++ toolsetup/vm/main.tf
@@ -70,13 +70,11 @@ resource "azurerm_linux_virtual_machine" "vm" {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-  delete_os_disk_on_termination = true
 
-  delete_data_disks_on_termination = true
+  # delete_os_disk_on_termination = true
+  # delete_data_disks_on_termination = true
 
-  storage_image_reference {
-    id = "/subscriptions/aa32da49-0603-4855-b55b-bfd4bcf7b16f/resourceGroups/project_rg/providers/Microsoft.Compute/images/test-devops-practice"
-  }
+    source_image_id_id = "/subscriptions/aa32da49-0603-4855-b55b-bfd4bcf7b16f/resourceGroups/project_rg/providers/Microsoft.Compute/images/test-devops-practice"
 
   #spot details
   priority        = "spot"
