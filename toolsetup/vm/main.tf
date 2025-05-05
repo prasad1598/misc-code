f3a8fd616ebb6597f5af6ebf0bb7a9e56a8a6239
muniprasad <muniprasaddiddi@gmail.com>
2025-05-05 11:27:29 +0530

    roboshop project update

--- toolsetup/vm/main.tf
+++ toolsetup/vm/main.tf
@@ -69,7 +69,7 @@ resource "null_resource" "vault" {
     inline = [
       "sudo dnf install python3.12 python3.12-pip -y",
       "sudo pip3.12 install ansible hvac",
-      "ansible-pull -i localhost, -U https://github.com/prasad1598/misc-code toolsetup/tool_setup.yml -e tool_name=$(var.name)"
+      "ansible-pull -i localhost, -U https://github.com/prasad1598/misc-code.git toolsetup/tool_setup.yml -e tool_name=$(var.name)"
     ]
   }
 }
