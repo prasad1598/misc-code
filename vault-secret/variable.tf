c53979a949e4f786b495106b657a819f80926fba
Muniprasad <muniprasaddiddi@gmail.com>
2025-05-22 16:29:28 +0530

    roboshop project update

--- vault-secret/variable.tf
+++ vault-secret/variable.tf
@@ -25,17 +25,17 @@ variable "values" {
       secret = "roboshop-infra"
       value = {
         catalogue_url = "http://catalogue-dev.prasaddevops.shop:8080",
-        user_url = "http://user-dev.prasaddevops.shop:8080",
-        cart_url = "http://cart-dev.prasaddevops.shop:8080",
-        shipping_url = "http://shipping-dev.prasaddevops.shop:8080",
-        payment_url = "http://payment-dev.prasaddevops.shop:8080"
+        user_url      = "http://user-dev.prasaddevops.shop:8080",
+        cart_url      = "http://cart-dev.prasaddevops.shop:8080",
+        shipping_url  = "http://shipping-dev.prasaddevops.shop:8080",
+        payment_url   = "http://payment-dev.prasaddevops.shop:8080"
       }
     }
 
     catalauge = {
       secret = "roboshop-infra"
       value = {
-        MONGO = true,
+        MONGO     = true,
         MONGO_URL = "mongodb://mongodb-dev.prasaddevops.shop:27017/catalogue"
       }
     }
@@ -43,8 +43,7 @@ variable "values" {
     cart = {
       secret = "roboshop-infra"
       value = {
-        user = roboshop,
-        REDIS_HOST = "redis-dev.prasaddevops.shop"
+        REDIS_HOST     = "redis-dev.prasaddevops.shop"
         CATALOGUE_HOST = "catalogue-dev.prasaddevops.shop"
         CATALOGUE_PORT = 8080
       }
@@ -53,8 +52,57 @@ variable "values" {
     user = {
       secret = "roboshop-infra"
       value = {
-        MONGO = true,
-        MONGO_URL = "mongodb://mongodb-dev.prasaddevops.shop:27017/catalogue"
+        MONGO     = true,
+        REDIS_URL = "redis://redis-dev.prasaddevops.shop"
+        MONGO_URL = "mongodb://mongodb-dev.prasaddevops.shop:27017/users"
+      }
+    }
+
+    dispatch = {
+      secret = "roboshop-infra"
+      value = {
+        AMQP_HOST = "rabbitmq-devprasaddevops.shop"
+        AMQP_USER = "roboshop"
+        AMQP_PASS = "roboshop123"
+      }
+    }
+
+    payment = {
+      secret = "roboshop-infra"
+      value = {
+        CART_HOST = "cart-dev.prasaddevops.shop"
+        CART_PORT = 8080
+        USER_HOST = "user-dev.prasaddevops.shop"
+        USER_PORT = 8080
+        AMQP_HOST = "rabbitmq-dev.prasaddevops.shop"
+        AMQP_USER = "roboshop"
+        AMQP_PASS = "roboshop123"
+      }
+    }
+
+
+    shipping = {
+      secret = "roboshop-infra"
+      value = {
+        CART_ENDPOINT = "cart-dev.prasaddevops.shop:8080"
+        DB_HOST       = "mysql-dev.prasaddevops.shop"
+        user          = "root"
+        password      = "RoboShop@1"
+      }
+    }
+
+    rabbitmq = {
+      secret = "roboshop-infra"
+      value = {
+        user = "roboshop"
+        password = "roboshop123"
+      }
+    }
+
+    mysql = {
+      secret = "roboshop-infra"
+      value = {
+        root-pass = "RoboShop@1"
       }
     }
   }
