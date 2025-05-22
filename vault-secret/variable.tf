9ce6ab09651109015321a08658eb22bc4403d24f
Muniprasad <muniprasaddiddi@gmail.com>
2025-05-22 15:09:27 +0530

    roboshop project update

--- vault-secret/variable.tf
+++ vault-secret/variable.tf
@@ -24,16 +24,37 @@ variable "values" {
     frontend = {
       secret = "roboshop-infra"
       value = {
-        username = "azuser",
-        password = "Devops@12345"
+        catalogue_url = "http://catalogue-dev.prasaddevops.shop:8080",
+        user_url = "http://user-dev.prasaddevops.shop:8080",
+        cart_url = "http://cart-dev.prasaddevops.shop:8080",
+        shipping_url = "http://shipping-dev.prasaddevops.shop:8080",
+        payment_url = "http://payment-dev.prasaddevops.shop:8080"
       }
     }
 
     catalauge = {
       secret = "roboshop-infra"
       value = {
-        username      = "azuser",
-        password       = "Devops@12345"
+        MONGO = true,
+        MONGO_URL = "mongodb://mongodb-dev.prasaddevops.shop:27017/catalogue"
+      }
+    }
+
+    cart = {
+      secret = "roboshop-infra"
+      value = {
+        user = roboshop,
+        REDIS_HOST = "redis-dev.prasaddevops.shop"
+        CATALOGUE_HOST = "catalogue-dev.prasaddevops.shop"
+        CATALOGUE_PORT = 8080
+      }
+    }
+
+    user = {
+      secret = "roboshop-infra"
+      value = {
+        MONGO = true,
+        MONGO_URL = "mongodb://mongodb-dev.prasaddevops.shop:27017/catalogue"
       }
     }
   }
